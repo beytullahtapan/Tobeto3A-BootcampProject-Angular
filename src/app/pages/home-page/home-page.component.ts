502d53306d0b487637a1a73def11250d341bc457
Beytullah Tapan <beytullahrodnes@gmail.com>
2024-04-24 19:25:25 +0300

    mobile navbar changed faq added footer added

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,11 +4,12 @@ import { SharedModule } from '../../shared/shared.module';
 import { BootcampListGroupComponent } from '../../features/components/bootcamps/bootcamp-list-group/bootcamp-list-group.component';
 import { JumbotronComponent } from '../../shared/components/jumbotron/jumbotron.component';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { FaqComponent } from '../../shared/components/faq/faq.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterModule,BootcampListGroupComponent,JumbotronComponent,NavbarComponent],
+  imports: [RouterModule,BootcampListGroupComponent,JumbotronComponent,NavbarComponent,FaqComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
