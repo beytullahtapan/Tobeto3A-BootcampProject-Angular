e328336f68066e8f70ee89fdd105383787377b07
arzualkann <arzualkann@outlook.com>
2024-05-02 03:05:47 +0300

    Update contact.service.ts

--- src/app/features/services/concretes/contact.service.ts
+++ src/app/features/services/concretes/contact.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
